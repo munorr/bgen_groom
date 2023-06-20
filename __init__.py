4073730eb6704603f5699370aa2ca745bfb7bc6d
munorr <137064603+munorr@users.noreply.github.com>
2023-06-20 13:52:47 -0500

    Inherit surface color Update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "BGEN Groom",
     "author": "Munorr",
-    "version": (1, 1, 0),
+    "version": (1, 1, 2),
     "blender": (3, 5, 0),
     "location": "View3D > N",
     "description": "Control parameters from B-GEN v2 geometry node hair system",
